492dbd27b7671c399008b87ec5a5db51160f01c5
TomTheToad <tomthetoad951@gmail.com>
2019-04-27 19:13:54 -0400

    type: feat
    Reworked whole of index html and css, including html generated by main.js, for a new silver / black /red theme

--- src/js/main.js
+++ src/js/main.js
@@ -164,31 +164,42 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  * Create restaurant HTML.
  */
 createRestaurantHTML = (restaurant) => {
-  const li = document.createElement('li');
+  const div = document.createElement('div');
+  div.className = 'card';
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
+  div.append(image);
+
+  const info = document.createElement('div');
+  info.className = 'restaurant-info';
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  info.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  info.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  info.append(address);
 
-  const more = document.createElement('a');
-  more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  // const moreContainer = document.createElement('div');
+  // moreContainer.className = "more";
 
-  return li
+  const link = document.createElement('a');
+  link.className = 'restaurant-link';
+  link.innerHTML = 'View Details';
+  link.href = DBHelper.urlForRestaurant(restaurant);
+
+  // moreContainer.append(button);
+  info.append(link);
+  div.append(info);
+
+  return div
 }
 
 /**
